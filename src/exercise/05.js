1f39af9beef4d57f6b3d672b7dcd5a73b2d0938d
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-04-11 06:42:05 +0330

    ex05: enable people to provide a custom reducer so they can have complete control over how state updates happens.

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -4,7 +4,10 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
-const callAll = (...fns) => (...args) => fns.forEach(fn => fn?.(...args))
+const callAll =
+  (...fns) =>
+  (...args) =>
+    fns.forEach(fn => fn?.(...args))
 
 function toggleReducer(state, {type, initialState}) {
   switch (type) {
@@ -20,14 +23,9 @@ function toggleReducer(state, {type, initialState}) {
   }
 }
 
-// 🐨 add a new option called `reducer` that defaults to `toggleReducer`
-function useToggle({initialOn = false} = {}) {
+function useToggle({initialOn = false, reducer = toggleReducer} = {}) {
   const {current: initialState} = React.useRef({on: initialOn})
-  // 🐨 instead of passing `toggleReducer` here, pass the `reducer` that's
-  // provided as an option
-  // ... and that's it! Don't forget to check the 💯 extra credit!
-  const [state, dispatch] = React.useReducer(toggleReducer, initialState)
-  const {on} = state
+  const [{on}, dispatch] = React.useReducer(reducer, initialState)
 
   const toggle = () => dispatch({type: 'toggle'})
   const reset = () => dispatch({type: 'reset', initialState})
@@ -60,13 +58,11 @@ function App() {
   const [timesClicked, setTimesClicked] = React.useState(0)
   const clickedTooMuch = timesClicked >= 4
 
+  // this is the custom reducer
   function toggleStateReducer(state, action) {
     switch (action.type) {
       case 'toggle': {
-        if (clickedTooMuch) {
-          return {on: state.on}
-        }
-        return {on: !state.on}
+        return clickedTooMuch ? {on: state.on} : {on: !state.on}
       }
       case 'reset': {
         return {on: false}
@@ -86,7 +82,7 @@ function App() {
       <Switch
         {...getTogglerProps({
           disabled: clickedTooMuch,
-          on: on,
+          on,
           onClick: () => setTimesClicked(count => count + 1),
         })}
       />
@@ -106,8 +102,3 @@ function App() {
 }
 
 export default App
-
-/*
-eslint
-  no-unused-vars: "off",
-*/
