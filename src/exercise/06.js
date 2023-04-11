ba575d7dc4350e68cd49d6bd1a5413e2712481e5
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-04-11 08:20:33 +0330

    ex0604: don't warn in production.

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -38,14 +38,16 @@ function useControlledSwitchWarning(
   const {current: wasControlled} = React.useRef(isControlled)
 
   React.useEffect(() => {
-    warning(
-      !(isControlled && !wasControlled),
-      `\`${componentName}\` is changing from uncontrolled to be controlled. Components should not switch from uncontrolled to controlled (or vice versa). Decide between using a controlled or uncontrolled \`${componentName}\` for the lifetime of the component. Check the \`${controlPropName}\` prop.`,
-    )
-    warning(
-      !(!isControlled && wasControlled),
-      `\`${componentName}\` is changing from controlled to be uncontrolled. Components should not switch from controlled to uncontrolled (or vice versa). Decide between using a controlled or uncontrolled \`${componentName}\` for the lifetime of the component. Check the \`${controlPropName}\` prop.`,
-    )
+    if (process.env.NODE_ENV !== 'production') {
+      warning(
+        !(isControlled && !wasControlled),
+        `\`${componentName}\` is changing from uncontrolled to be controlled. Components should not switch from uncontrolled to controlled (or vice versa). Decide between using a controlled or uncontrolled \`${componentName}\` for the lifetime of the component. Check the \`${controlPropName}\` prop.`,
+      )
+      warning(
+        !(!isControlled && wasControlled),
+        `\`${componentName}\` is changing from controlled to be uncontrolled. Components should not switch from controlled to uncontrolled (or vice versa). Decide between using a controlled or uncontrolled \`${componentName}\` for the lifetime of the component. Check the \`${controlPropName}\` prop.`,
+      )
+    }
   }, [componentName, controlPropName, isControlled, wasControlled])
 }
 
@@ -61,10 +63,12 @@ function useOnChangeReadOnlyWarning(
 ) {
   const isControlled = controlPropValue != null
   React.useEffect(() => {
-    warning(
-      !(!hasOnChange && isControlled && !readOnly),
-      `A \`${controlPropName}\` prop was provided to \`${componentName}\` without an \`${onChangeProp}\` handler. This will result in a read-only \`${controlPropName}\` value. If you want it to be mutable, use \`${initialValueProp}\`. Otherwise, set either \`${onChangeProp}\` or \`${readOnlyProp}\`.`,
-    )
+    if (process.env.NODE_ENV !== 'production') {
+      warning(
+        !(!hasOnChange && isControlled && !readOnly),
+        `A \`${controlPropName}\` prop was provided to \`${componentName}\` without an \`${onChangeProp}\` handler. This will result in a read-only \`${controlPropName}\` value. If you want it to be mutable, use \`${initialValueProp}\`. Otherwise, set either \`${onChangeProp}\` or \`${readOnlyProp}\`.`,
+      )
+    }
   }, [
     componentName,
     controlPropName,
